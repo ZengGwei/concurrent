0e66d274741115ec81ad4af068e8c5171df942f1
zenggw <791440867@qq.com>
2019-06-10 18:21:01 +0800

    concurrentHashMap

--- src/com/zgw/chart6JavaConcurrenContextfirmwork/ConcurrentHashMapDemo.java
+++ src/com/zgw/chart6JavaConcurrenContextfirmwork/ConcurrentHashMapDemo.java
@@ -5,19 +5,39 @@ import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * 〈ConcurrentHashMap 的实现原理〉
+ * 请思考并回答以下问题
+ * 1.ConcurrentHashMap1.8中是基于什么机制来保证线程安全性的
+ *    答：  通过锁分离技术，由JDK7中锁segment 段 变更成 锁 Node节点 ；细化了锁的粒度
  *
- * @author gw.Zeng
- * @create 2019/6/8
- * @since 1.0.0
+ * 2.ConcurrentHashMap通过get方法获取数据的时候，是否需要通过加锁来保证数据的可见性？为什么？
+ *    答： 不需要，数据put操作是CAS操作，已更新数据到主内存 。
+ * 3. ConcurrentHashMap1.7和ConcurrentHashMap1.8有哪些区别？
+ *   答： 1. 不采用segment而采用node，锁住node来实现减小锁粒度。
+ *        2.设计了MOVED状态 当resize的中过程中 线程2还在put数据，线程2会帮助resize。
+ *       3.使用3个CAS操作（tabAt(Node<K,V>[] tab, int i)、casTabAt（Node<K,V>[] tab, int i,Node<K,V> c, Node<K,V> v）、setTabAt(Node<K,V>[] tab, int i, Node<K,V> v)）
+ *         来确保node的一些操作的原子性，这种方式代替了锁。
+ *      4.sizeCtl的不同值来代表不同含义，起到了控制的作用。
+ * 4.ConcurrentHashMap1.8为什么要引入红黑树？
+ *   答：解决hash冲突 ，节点链表过长，造成读取数据效率低下，所以引入红黑树。
  */
+
 public class ConcurrentHashMapDemo {
     final HashMap<String, String> map = new HashMap<String, String>(2);
     public static void main(String[] args) {
         //ConcurrentHashMap的锁分段技术可有效提升并发访问率
-        ConcurrentHashMap<String,String> map =new ConcurrentHashMap(25);
+        ConcurrentHashMap<String,String> map =new ConcurrentHashMap(55);//this.sizeCtl = cap; 只设置了一个容量  a power of two table size
 
+
+        /**
+         *
+         */
         map.put("name","zgw");
 
+
+        map.size();
+
+
+        String name = map.get("name");
     }
 
 
--- src/com/zgw/connectionPool/ThreadPoolDome.java
+++ src/com/zgw/connectionPool/ThreadPoolDome.java
@@ -5,10 +5,17 @@ import java.util.concurrent.Executors;
 
 /**
  * 〈〉
- *
- * @author gw.Zeng
- * @create 2019/6/4
- * @since 1.0.0
+ *为什么要使用线程池？
+ *1/、 Executors提供的四种线程池:newSingleThreadExecutor,newFixedThreadPool,newCachedThreadPool,newScheduledThreadPool ，请说出他们的区别以及应用场景
+ *2、线程池有哪几种工作队列？
+ * 3、线程池默认的拒绝策略有哪些
+ * 4、如何理解有界队列和无解队列
+ *5、 线程池是如何实现线程回收的？ 以及核心线程能不能被回收？
+ *6、 FutureTask是什么
+ * 7、Thread.sleep(0)的作用是什么
+ * 8、如果提交任务时，线程池队列已满，这时会发生什么
+ * 9、如果一个线程池中还有任务没有执行完成，这个时候是否允许被外部中断？
+
  */
 public class ThreadPoolDome {
 
